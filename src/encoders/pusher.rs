70fccbf380dcb6d26276870923b96ca632497966
aegroto <aegroto@protonmail.com>
2026-05-28 17:04:45 +0200

    feat: simpler pusher

--- src/encoders/pusher.rs
+++ src/encoders/pusher.rs
@@ -24,7 +24,7 @@ where
     T: AVFrameFiller<F> + Send,
     F: FFMpegCodec + Send + 'static,
 {
-    async fn process(&mut self, mut frame_data: F) -> Option<F> {
+    async fn process(&mut self, frame_data: F) -> Option<F> {
         if self.eof_processed {
             return None;
         }
@@ -34,7 +34,6 @@ where
         if frame_data.is_eof() {
             log::debug!("EncoderPusher: EOF, flushing encoder");
             encode_context.send_frame(None).ok();
-            drain_packets(&mut encode_context, &mut frame_data);
             self.eof_processed = true;
             return Some(frame_data);
         }
@@ -49,45 +48,16 @@ where
             .scaled_frame_mut()
             .set_pts(frame_data.get_frame_id());
 
-        let mut sent = false;
-        while !sent {
-            match encode_context.send_frame(Some(self.scaler.scaled_frame())) {
-                Ok(()) => {
-                    sent = true;
-                }
-                Err(RsmpegError::SendFrameAgainError) => {
-                    drain_packets(&mut encode_context, &mut frame_data);
-                }
-                Err(e) => {
-                    log::warn!("EncoderPusher: send_frame error: {:?}", e);
-                    frame_data.report_codec_error();
-                    sent = true;
-                }
+        match encode_context.send_frame(Some(self.scaler.scaled_frame())) {
+            Ok(()) => {}
+            Err(RsmpegError::SendFrameAgainError) => {
+                log::warn!("EncoderPusher: encoder not ready, dropping frame");
+            }
+            Err(e) => {
+                log::warn!("EncoderPusher: send_frame error: {:?}", e);
             }
         }
 
         Some(frame_data)
-    }
-}
-
-fn drain_packets<F: FFMpegCodec>(encode_context: &mut AVCodecContext, frame_data: &mut F) {
-    loop {
-        match encode_context.receive_packet() {
-            Ok(packet) => {
-                let data =
-                    unsafe { std::slice::from_raw_parts(packet.data, packet.size as usize) };
-                frame_data.write_packet_data(data);
-                frame_data.set_frame_id(packet.pts);
-            }
-            Err(RsmpegError::EncoderDrainError) => break,
-            Err(RsmpegError::EncoderFlushedError) => {
-                frame_data.report_flush_error();
-                break;
-            }
-            Err(e) => {
-                log::warn!("EncoderPusher: drain receive_packet error: {:?}", e);
-                break;
-            }
-        }
     }
 }
